1ee59b675b40294528656ee3549b1e5277279d99
Elias Ehmann <eliaz.develops@gmail.com>
2020-08-13 14:36:03 +0200

    added stuff

--- zurprisor/src/app/app.component.ts
+++ zurprisor/src/app/app.component.ts
@@ -1,18 +1,42 @@
-import { Component } from '@angular/core';
-import { Router } from "@angular/router"
+import { Component, ViewEncapsulation, OnInit, ViewChild } from '@angular/core';
+
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor() {
+    //called first time before the ngOnInit()
+  }
 
   title = 'zurprisor';
-  constructor(private router: Router) { }
+
+  public cdTime =
+    {
+      "stopTime": 1598104800,
+      "format": 'dd:HH:mm:ss',
+
+    };
+
+  public countDownIsVisible: boolean;
+
+  ngOnInit() {
+    this.countDownIsVisible = true;
+
+    var ts = Math.round((new Date()).getTime() / 1000);
+
+    if (this.cdTime.stopTime - ts < 0) {
+      this.countDownIsVisible = false;
+    }
+
+  }
 }
 
 
--- zurprisor/src/app/app.module.ts
+++ zurprisor/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CountdownModule } from 'ngx-countdown';
+
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CountdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
